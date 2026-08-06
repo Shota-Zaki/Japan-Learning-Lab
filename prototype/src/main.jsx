31e67056d4649cb795831f3a462d94d7331141e1
Shota-Zaki <shota690@gmail.com>
2026-08-06 12:20:04 +0900

    style: load compact practice filters

--- prototype/src/main.jsx
+++ prototype/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { AppV5 } from "./AppV5.jsx";
 import "./styles.css";
 import "./fe-v5.css";
+import "./fe-filter-variants.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
