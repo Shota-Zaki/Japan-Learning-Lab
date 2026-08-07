0c4fe51364f1d7049ced6cf5e720311313d3c7f3
Shota-Zaki <shota690@gmail.com>
2026-08-07 09:56:54 +0900

    feat: expose FE filter layout for validation

--- prototype/src/main.jsx
+++ prototype/src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { AppV5 } from "./AppV5.jsx";
+import { resolveFeFilterLayoutVariant } from "./feFilterLayout.js";
 import "./styles.css";
 import "./fe-v5.css";
 import "./fe-filter-variants.css";
 import "./fe-result-review.css";
 import "./fe-session-enhancements.css";
+
+document.documentElement.dataset.feFilterLayout = resolveFeFilterLayoutVariant(window.location.search);
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
