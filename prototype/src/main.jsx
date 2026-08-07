4e3b002b08610ecbedf37d180817b40005e13ed8
Shota-Zaki <shota690@gmail.com>
2026-08-07 23:20:06 +0900

    style: load FE lesson styles [skip ci]

--- prototype/src/main.jsx
+++ prototype/src/main.jsx
@@ -4,6 +4,7 @@ import { AppV5 } from "./AppV5.jsx";
 import { resolveFeFilterLayoutVariant } from "./feFilterLayout.js";
 import "./styles.css";
 import "./fe-v5.css";
+import "./fe-lesson.css";
 import "./fe-filter-variants.css";
 import "./fe-result-review.css";
 import "./fe-session-enhancements.css";
